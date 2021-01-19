edeb3747afb2fd95eff77401ad81d86f9f5614ea
ChoKiyeon <qsky8520@gmail.com>
2021-01-20 02:32:30 +0900

    algorithm

--- sparta_algorithm/week_4/homework/03_get_all_ways_of_theater_seat.py
+++ sparta_algorithm/week_4/homework/03_get_all_ways_of_theater_seat.py
@@ -1,9 +1,44 @@
 seat_count = 9
 vip_seat_array = [4, 7]
 
+memo = {
+    1: 1,
+    2: 2
+}
+
+
+def fibo(n, fibo_memo):
+    if n == 0:
+        return 1
+    if n in fibo_memo:
+        return fibo_memo[n]
+
+    plus_fibo = fibo(n - 1, fibo_memo) + fibo(n - 2, fibo_memo)
+    fibo_memo[n] = plus_fibo
+
+    return plus_fibo
+
 
 def get_all_ways_of_theater_seat(total_count, fixed_seat_array):
-    return
+    start_index = [0]
+    ways_of_seat = 1
+
+    for index in range(len(fixed_seat_array) + 1):
+        count = 0
+
+        if index == len(fixed_seat_array):
+            vip_seat = total_count + 1
+        else:
+            vip_seat = fixed_seat_array[index]
+
+        for seat in range(start_index.pop() + 1, total_count + 1):
+            if seat == vip_seat:
+                start_index.append(seat)
+                break
+            count += 1
+
+        ways_of_seat *= fibo(count, memo)
+    return ways_of_seat
 
 
 # 12가 출력되어야 합니다!
