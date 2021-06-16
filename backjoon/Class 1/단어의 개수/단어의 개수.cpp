30b2a4e1206406ec1d9fb5fbb1cb9f6e8923375f
unknown <hsky8520@cbnu.ac.kr>
2021-06-16 22:52:49 +0900

    단어의 개수

---
+++ backjoon/Class 1/단어의 개수/단어의 개수.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin, str);
+
+    int cnt = 0;
+    int n = str.length();
+    for (int i = 0; i < n; i++)
+    {
+        if (n == 1 && str[i] == ' ')
+        {
+            cout << 0;
+            return 0;
+        }
+
+        if ((i == 0 || i == n - 1) && str[i] == ' ')
+            continue;
+
+        if (str[i] == ' ')
+            cnt++;
+    }
+
+    cout << cnt + 1;
+
+    return 0;
+}
