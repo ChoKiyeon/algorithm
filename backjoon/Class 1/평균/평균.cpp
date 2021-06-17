3895db3c2f2ed353fed6072d9631afd5b6bb8a18
unknown <hsky8520@cbnu.ac.kr>
2021-06-17 22:11:13 +0900

    평균

---
+++ backjoon/Class 1/평균/평균.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	double grade[1000];
+
+	double n;
+	cin >> n;
+
+	int max_idx = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> grade[i];
+
+		if (grade[max_idx] < grade[i])
+			max_idx = i;
+	}
+
+	double sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += grade[i] / grade[max_idx] * 100.0;
+	}
+
+	cout << fixed;
+	cout.precision(6);
+	cout << sum / n;
+
+	return 0;
+}
