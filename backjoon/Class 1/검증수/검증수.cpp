294141034c20f2a1bf334248bfdc6d1a91f40871
unknown <hsky8520@cbnu.ac.kr>
2021-06-19 23:25:59 +0900

    검증수

---
+++ backjoon/Class 1/검증수/검증수.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n[5];
+	int num = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> n[i];
+		n[i] *= n[i];
+		num += n[i];
+	}
+
+	cout << num % 10;
+
+	return 0;
+}
