6f8710686a12596001bc2ccf63e34d2cf9954ff5
unknown <hsky8520@cbnu.ac.kr>
2021-06-20 22:51:39 +0900

    윤년

---
+++ backjoon/Class 1/윤년/윤년.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int is_LeapYear(int year)
+{
+    if (year % 4 != 0)
+        return 0;
+
+    if (year % 100 != 0)
+        return 1;
+
+    if (year % 400 == 0)
+        return 1;
+
+    return 0;
+}
+
+int main()
+{
+    int year;
+    cin >> year;
+
+    cout << is_LeapYear(year);
+}
