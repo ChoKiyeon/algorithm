2ad019c906382b208814b4f5504bb912970d433a
ChoKiyeon <qsky8520@gmail.com>
2021-07-06 23:03:01 +0900

    최소, 최대

---
+++ backjoon/Class 1/최소, 최대/최소, 최대.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n;
+	cin >> n;
+
+	int min = 1000000;
+	int max = -1000000;
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+
+		if (num > max)
+			max = num;
+
+		if (num < min)
+			min = num;
+	}
+
+	cout << min << ' ' << max;
+
+	return 0;
+}
