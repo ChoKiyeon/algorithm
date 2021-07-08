4830a07143b6771b06be2729d0bc14eddc258495
ChoKiyeon <qsky8520@gmail.com>
2021-07-08 18:43:27 +0900

    최댓값

---
+++ backjoon/Class 1/최댓값/최댓값.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n;
+	int max = 0, max_idx = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		cin >> n;
+		if (n > max)
+		{
+			max = n;
+			max_idx = i + 1;
+		}
+	}
+
+	cout << max << '\n' << max_idx;
+
+	return 0;
+}
