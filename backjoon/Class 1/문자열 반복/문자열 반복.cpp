df69339e8895db3ff36fd55603197efe9b0e34ed
ChoKiyeon <qsky8520@gmail.com>
2021-07-08 18:42:55 +0900

    문자열 반복

---
+++ backjoon/Class 1/문자열 반복/문자열 반복.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int t;
+	cin >> t;
+
+	for(int i = 0; i < t; i++)
+	{
+		int r;
+		string str;
+		cin >> r >> str;
+
+		for (int i = 0; i < str.size(); i++)
+		{
+			for (int j = 0; j < r; j++)
+			{
+				cout << str[i];
+			}
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
