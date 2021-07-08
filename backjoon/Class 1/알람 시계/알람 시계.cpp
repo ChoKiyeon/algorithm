7418c560b78a1f763a0a06aa9df225ac79795bc9
ChoKiyeon <qsky8520@gmail.com>
2021-07-08 18:43:15 +0900

    알람 시계

---
+++ backjoon/Class 1/알람 시계/알람 시계.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int h, m;
+	cin >> h >> m;
+
+	int cvt_M = 60 * h + m - 45;
+	int H = cvt_M / 60;
+	int M = abs(cvt_M % 60);
+
+	if (cvt_M + 45 < 45)
+	{
+		H = 23;
+		M = 60 - M;
+	}
+	
+	cout << H << ' ' << M;
+
+	return 0;
+}
