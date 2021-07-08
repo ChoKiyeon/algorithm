6053f52f3a7420f8e7caaaaeaa06a4482d3edb79
ChoKiyeon <qsky8520@gmail.com>
2021-07-08 18:43:05 +0900

    숫자의 개수

---
+++ backjoon/Class 1/숫자의 개수/숫자의 개수.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	int n = a * b * c;
+	int dat[10] = { 0 };
+	while (n != 0)
+	{
+		dat[n % 10]++;
+		n /= 10;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << dat[i] << '\n';
+	}
+
+	return 0;
+}
