eff7a14b41de1ed206fa9eb0e8aabc7cd0a1507d
ChoKiyeon <qsky8520@gmail.com>
2021-07-12 14:30:14 +0900

    음계

---
+++ backjoon/Class 1/음계/음계.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	string str = "";
+	for (int i = 0; i < 8; i++)
+	{
+		char ch;
+		cin >> ch;
+
+		str += ch;
+	}
+
+	if (str == "12345678")
+		cout << "ascending";
+	else if (str == "87654321")
+		cout << "descending";
+	else
+		cout << "mixed";
+
+	return 0;
+}
