023da884d1718ce37cf01f52bc79a1d0c54be4d0
ChoKiyeon <qsky8520@gmail.com>
2021-07-12 14:29:44 +0900

    OX퀴즈

---
+++ backjoon/Class 1/OX퀴즈/OX퀴즈.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        string result;
+        cin >> result;
+
+        int idx = 0;
+        int score = 0;
+        while (1)
+        {
+            int cnt = 0;
+            while (result[idx] == 'O')
+            {
+                cnt++;
+                score += cnt;
+                idx++;
+            }
+
+            idx = result.find('O', idx);
+
+            if (idx == -1)
+                break;
+        }
+
+        cout << score << '\n';
+    }
+}
