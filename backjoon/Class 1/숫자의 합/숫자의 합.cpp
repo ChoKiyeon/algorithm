a8cbbca06203df17247333d1db0cbebf1fa4885e
ChoKiyeon <qsky8520@gmail.com>
2021-07-12 14:29:32 +0900

    숫자의 합

---
+++ backjoon/Class 1/숫자의 합/숫자의 합.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    string sn;
+    cin >> sn;
+
+    int result = 0;
+    for (int i = 0; i < n; i++)
+    {
+        result += sn[i] - '0';
+    }
+
+    cout << result;
+}
