09b6bf95a65e3784ff1a04a38d017fde093235b7
ChoKiyeon <qsky8520@gmail.com>
2021-07-12 14:29:51 +0900

    나머지

---
+++ backjoon/Class 1/나머지/나머지.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int dat[42] = { 0 };
+    for (int i = 0; i < 10; i++)
+    {
+        int n;
+        cin >> n;
+
+        dat[n % 42]++;
+    }
+
+    int cnt = 0;
+    for(int i = 0; i < 42; i++)
+    {
+        if (dat[i])
+            cnt++;
+    }
+
+    cout << cnt;
+
+    return 0;
+}
