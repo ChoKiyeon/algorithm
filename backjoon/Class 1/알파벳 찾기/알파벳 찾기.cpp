ebbaeebf49760ffc8a2c863c182ffb23022dd7fa
ChoKiyeon <qsky8520@gmail.com>
2021-07-12 14:30:06 +0900

    알파벳 찾기

---
+++ backjoon/Class 1/알파벳 찾기/알파벳 찾기.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int dat[26];
+    fill(dat, dat + 26, -1);
+
+    string str;
+    cin >> str;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (dat[str[i] - 'a'] != -1)
+            continue;
+
+        dat[str[i] - 'a'] = i;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        cout << dat[i] << ' ';
+    }
+
+    return 0;
+}
