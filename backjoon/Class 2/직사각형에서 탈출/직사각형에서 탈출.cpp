fc32eb3e28f8649b457e2bfa516f43e3cc8fdaf7
ChoKiyeon <qsky8520@gmail.com>
2021-07-13 21:05:18 +0900

    직사각형에서 탈출

---
+++ backjoon/Class 2/직사각형에서 탈출/직사각형에서 탈출.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int x, y, w, h;
+	cin >> x >> y >> w >> h;
+
+	int left_DX = x;
+	int right_DX = w - x;
+	int up_DX = h - y;
+	int down_DX = y;
+	
+	int DX[4] = { left_DX, right_DX, up_DX, down_DX };
+	int min_idx = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		if (DX[min_idx] > DX[i])
+			min_idx = i;
+	}
+
+	cout << DX[min_idx];
+
+	return 0;
+}
